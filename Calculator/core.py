7b1b919d25d6e3ac3b71f66b86a27ef5cbbd38a6
ChaoticSteve <tuninpavel@gmail.com>
2021-11-16 23:02:57 +0600

    update core.py
    add method 'winner', atr 'name'

--- Calculator/core.py
+++ Calculator/core.py
@@ -1,5 +1,6 @@
 class Character():
-    def __init__(self, health, armor, damage):
+    def __init__(self, name, health, armor, damage):
+        self.name = name
         self.health = health
         self.armor = armor
         self.damage = damage
@@ -17,8 +18,22 @@ class Character():
         result = [random.randint(1, int(roll[1])) for _ in range(int(roll[0]))]
         return sum(result)
 
+    def winner(self, other):
+        turn = True
+        i = 1
+        while self.health > 0 or other.health > 0:
+            if turn:
+                self.attack(other)
+                print(f'{i} round:{self.name} attack {other.name}')
+                turn = False
+            else:
+                other.attack(self)
+                print(f'{i} round:{other.name} attack {self.name}')
+                turn = True
+            i += 1
+
 
 if __name__ == '__main__':
-    hero = Character(10, 15, '1d8')
-    villain = Character(15, 10, '1d6')
-    print(hero.attack(villain))
+    hero = Character('Jhon', 10, 15, '1d8')
+    villain = Character('Mike', 15, 10, '1d6')
+    hero.winner(villain)
